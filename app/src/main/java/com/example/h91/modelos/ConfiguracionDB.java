82561eac46bdae2afd758c3b17c7729f1ae95f58
jcumplido <josecumplido82@gmail.com>
2021-05-11 08:22:07 +0200

    conexion con bbdd externa

--- app/src/main/java/com/example/h91/modelos/ConfiguracionDB.java
+++ app/src/main/java/com/example/h91/modelos/ConfiguracionDB.java
@@ -13,13 +13,13 @@ import java.security.SecureRandom;
 public class ConfiguracionDB {
 
     //PARA LOCAL
-    public static final String HOSTDB = "10.0.2.2";
-    public static final String NOMBREDB = "empleados";
-    public static final String USUARIODB = "root";
-    public static final String CLAVEDB = "";
-    private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    public static final String PUERTOMYSQL = "3306";
-    public static final String URLMYSQL = "jdbc:mysql://" + HOSTDB + ":" + PUERTOMYSQL + "/" + NOMBREDB + OPCIONES;
+   // public static final String HOSTDB = "10.0.2.2";
+   // public static final String NOMBREDB = "empleados";
+   // public static final String USUARIODB = "root";
+   // public static final String CLAVEDB = "";
+   // private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+   // public static final String PUERTOMYSQL = "3306";
+   // public static final String URLMYSQL = "jdbc:mysql://" + HOSTDB + ":" + PUERTOMYSQL + "/" + NOMBREDB + OPCIONES;
 
     //----------------------------------------------------------------------------------------------------
 
@@ -42,13 +42,13 @@ public class ConfiguracionDB {
     //PARA SERVER INSTITUTO
 
 
-    //public static final String HOSTDB="infsalinas.sytes.net";
-    //public static final String NOMBREDB="empleados";
-    //public static final String USUARIODB="usuario";
-    //public static final String CLAVEDB="dam1234";
-    //private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
-    //public static final String PUERTOMYSQL="5306";
-    //public static final String URLMYSQL = "jdbc:mysql://"+ HOSTDB + ":" + PUERTOMYSQL+"/" + NOMBREDB + OPCIONES;
+    public static final String HOSTDB="infsalinas.sytes.net";
+    public static final String NOMBREDB="empleados";
+    public static final String USUARIODB="usuario";
+    public static final String CLAVEDB="dam1234";
+    private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+    public static final String PUERTOMYSQL="5306";
+    public static final String URLMYSQL = "jdbc:mysql://"+ HOSTDB + ":" + PUERTOMYSQL+"/" + NOMBREDB + OPCIONES;
 
     //variables staticas para usar en toda la APP
     public static String pass = "Madrid2021";
